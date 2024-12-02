0d6c706a25e5cf85906d5669d1494a3390ec48f4
Christian Francia <christian.francia@tui.com>
2024-12-02 09:20:37 +0100

    wip

---
+++ day1/day1.go
@@ -0,0 +1,8 @@
+package day1
+
+/*
+- sort the left and right column smallest to biggest
+- compare the smallest of the left column to the smallest of the right column
+- create a third column with the difference between the two
+- add column 3 to the total
+*/
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ctfrancia/AOC2024
+
+go 1.21.0
